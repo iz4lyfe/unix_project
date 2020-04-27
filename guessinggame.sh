32586eaa00c3f7462e4bae70acaf765bd1416467
iz4lyfe <iz4lyfe@gmail.com>
2020-04-27 11:47:43 -0700

    cleaned up code

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 answer=$(ls -1 | wc -l)
-attempts=0
 
 function be_my_guess() {
    read guess
@@ -25,20 +24,3 @@ echo "Welcome to 'Be My Guess'!  Please guess the number of files in this direct
 while [[ $guess -ne $answer ]]
 do be_my_guess
 done
-
-#while true
-#do be_my_guess
-#   guesses=("${guesses[@]}" $guess)
-#   echo "You have guessed ${#array[@]} time(s).  Your previous guesses were: ${guesses[@]}"
-#   read guess
-#   if [[ $guess -gt $answer ]]
-#      then echo "Your guess of $guess is too high.  Please try again."
-#   elif [[ $guess -lt $answer ]]
-#      then echo "Your guess of $guess is too low.  Please try again."
-#   elif [[ $guess -eq $answer ]]
-#      then echo "Congratulations!  $guess is the correct answer!"
-#      break
-#   else
-#           echo "Your guess is out of range."
-#   fi
-#done
